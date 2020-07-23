e540642caa0587e8ece6bca9df0befe2c7333156
patricoferris <patrickferris17@gmail.com>
2020-07-23 15:32:26 +0100

    Change css path and add some index

--- explore/lib/html.ml
+++ explore/lib/html.ml
@@ -9,7 +9,7 @@ let wrap_body ~title ~body =
       <title>|} (Html.txt title)
       {|</title>
       <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono&family=Ubuntu:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet">
-      <link rel=stylesheet href="css/main.css"/>
+      <link rel=stylesheet href="static/css/main.css"/>
       <link rel="stylesheet"  href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.0.0/styles/gruvbox-dark.min.css">
       <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.0.0/highlight.min.js"></script>
